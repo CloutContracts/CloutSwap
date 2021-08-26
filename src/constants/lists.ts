033111ebe99ca9e568dd89289857042efa2eaf88
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-26 19:10:22 -0400

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -6,8 +6,6 @@ const CCS_BSC_LIST = 'https://gateway.pinata.cloud/ipfs/QmXVYDsbaMcK8NjqjMx1XDFE
 const BIRB_LIST = 'https://gateway.pinata.cloud/ipfs/QmUzqasKre5PMe2gAvnzy7dPSkbckF9wFf4dU9z1GfMWJb'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 
-// export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
-
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [GEMINI_LIST, BIRB_LIST, CCS_BSC_LIST, CCS_ETC_LIST, CCS_LIST]
 
