28e96a87cfea1740405a3b7816fb94aaa9a0ab65
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-26 19:07:39 -0400

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -15,7 +15,6 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   CCS_BSC_LIST,
   CCS_ETC_LIST,
   CCS_LIST,
-  //  ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
 // default lists to be 'active' aka searched across
