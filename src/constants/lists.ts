fb62996a4330c30338fc9232f81ab5a8508a635e
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-26 20:56:20 -0400

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -6,10 +6,10 @@ const CCS_BSC_LIST = 'https://gateway.pinata.cloud/ipfs/QmXVYDsbaMcK8NjqjMx1XDFE
 const BIRB_LIST = 'https://gateway.pinata.cloud/ipfs/QmUzqasKre5PMe2gAvnzy7dPSkbckF9wFf4dU9z1GfMWJb'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 
-export const UNSUPPORTED_LIST_URLS: string[] = [CCS_BSC_LIST, CCS_ETC_LIST, CCS_LIST]
+// export const UNSUPPORTED_LIST_URLS: string[] = [CCS_BSC_LIST, CCS_ETC_LIST, CCS_LIST]
 
 // lower index == higher priority for token import
-export const DEFAULT_LIST_OF_LISTS: string[] = [GEMINI_LIST, BIRB_LIST, ...UNSUPPORTED_LIST_URLS]
+export const DEFAULT_LIST_OF_LISTS: string[] = [GEMINI_LIST, BIRB_LIST, CCS_BSC_LIST, CCS_ETC_LIST, CCS_LIST]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [...UNSUPPORTED_LIST_URLS]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [GEMINI_LIST, BIRB_LIST, CCS_BSC_LIST, CCS_ETC_LIST, CCS_LIST]
