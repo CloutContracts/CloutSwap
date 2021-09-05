63fa06c1861da7391cef4d7f08d19cda2fa7f76c
Andrew Kamal <kamalandrew55@gmail.com>
2021-09-04 20:43:28 -0400

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -121,7 +121,7 @@ const InternalMenuItem = styled(Link)`
   }
 `
 
-const CODE_LINK = 'https://github.com/birbtoken/birbswap-interface'
+const CODE_LINK = 'https://github.com/CloutContracts/CloutSwap'
 
 export default function Menu() {
   const { account } = useActiveWeb3React()
