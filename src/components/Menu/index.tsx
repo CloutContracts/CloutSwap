f3c1649b4a505b45201ffe16e1e09988ffdb75ba
Andrew Kamal <kamalandrew55@gmail.com>
2021-09-04 20:55:51 -0400

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -121,7 +121,7 @@ const InternalMenuItem = styled(Link)`
   }
 `
 
-const CODE_LINK = 'https://github.com/CloutContracts/CloutSwap'
+const CODE_LINK = 'https://cloutcontracts.net'
 
 export default function Menu() {
   const { account } = useActiveWeb3React()
